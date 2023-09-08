51498184f765fa6e1a87682438eee06907f6d819
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-08 17:08:45 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class config:
 
     API_ID = "8623612"
     API_HASH = "06ea2889c5517eb64017b032d667e29f"
-    BOT_TOKEN = "5905186632:AAFpiou14Ae6IzBTQzU_WrOwBPYmalClJ-E"
+    BOT_TOKEN = "6265217004:AAE_yot4SqUFrvBa1IJXgRy-kaeZm9z3Xwk"
 
     SUDO_USERS = 5954494174
     MONGO_URI = 'mongodb+srv://marcopetrali657:marcopetrali657@cluster0.dwztggn.mongodb.net/?retryWrites=true&w=majority'
