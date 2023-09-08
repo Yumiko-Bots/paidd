961ca7787f530093887802e73778f4baafc0775d
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-08 17:05:28 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,13 @@
+from pyrogram import Client, filters, idle
+from Config import config
 
+corn = Client(
+       ":corn:",
+       api_id=config.API_ID,
+       api_hash=config.API_HASH,
+       bot_token=config.BOT_TOKEN,
+       plugins=dict(root="plugins.modules")
+)
+print("Bot started!")
+corn.start()
+idle()
