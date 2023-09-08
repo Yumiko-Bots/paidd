f62cd5b900da02a64967af2103584d1c24660922
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-08 17:07:30 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+from os import path, getenv
+
+class config:
+
+    API_ID = "8623612"
+    API_HASH = "06ea2889c5517eb64017b032d667e29f"
+    BOT_TOKEN = "5905186632:AAFpiou14Ae6IzBTQzU_WrOwBPYmalClJ-E"
+
+    SUDO_USERS = 5954494174
+    MONGO_URI = 'mongodb+srv://marcopetrali657:marcopetrali657@cluster0.dwztggn.mongodb.net/?retryWrites=true&w=majority'
