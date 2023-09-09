b3ba2289c93954dba98464f20c3c136c3792ad38
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-09 20:57:33 +0530

    Update batch.py

--- plugins/modules/batch.py
+++ plugins/modules/batch.py
@@ -1,99 +1,176 @@
+import asyncio
+import datetime
+import logging
+from database.users import get_user
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-import base64
-import re
-import pyromod
-from pyromod import listen
-from plugins import corn 
+from pyrogram.errors import ChatWriteForbidden, PeerIdInvalid, FloodWait
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+from plugins.modules.helpers import main_convertor_handler, update_stats, user_api_check, AsyncIter, temp
 
-SUDO_USERS = [5954494174, 5857041668]
 
-async def encode(string):
-    string_bytes = string.encode("ascii")
-    base64_bytes = base64.urlsafe_b64encode(string_bytes)
-    base64_string = (base64_bytes.decode("ascii")).strip("=")
-    return base64_string
+SUDO_USERS = [5954494174,5857041668]
 
-async def get_message_id(client, message):
-    if message.forward_from_chat:
-        if message.forward_from_chat.id == client.db_channel.id:
-            return message.forward_from_message_id
-        else:
-            return 0
-    elif message.forward_sender_name:
-        return 0
-    elif message.text:
-        pattern = r"https://t.me/(?:c/)?(.*)/(\d+)"
-        matches = re.match(pattern, message.text)
-        if not matches:
-            return 0
-        channel_id = matches.group(1)
-        msg_id = int(matches.group(2))
-        if channel_id.isdigit():
-            if f"-100{channel_id}" == str(client.db_channel.id):
-                return msg_id
-        else:
-            if channel_id == client.db_channel.username:
-                return msg_id
+logger = logging.getLogger(__name__)
+
+lock = asyncio.Lock()
+
+cancel_button = [[InlineKeyboardButton(
+    "Cancel 🔐", callback_data="cancel_process")]]
+
+
+@Client.on_message(filters.private & filters.command("batch") & filters.user(SUDO_USERS))
+async def batch(c, m: Message):
+
+    if m.from_user.id not in SUDO_USERS:
+        return await m.reply_text("Works only for Sudo users")
+
+    user_id = m.from_user.id
+    user = await get_user(user_id)
+
+    vld = await user_api_check(user)
+
+    if len(m.command) < 2:
+        await m.reply_text("""Need to shorten or convert links from all of your channel's posts? I've got you covered! Just make me an admin in your channel and use the following command:
+
+<code>/batch [channel id or username]</code>
+
+For example: <code>/batch -100xxx</code>
+
+I'll handle the rest and get those links shortened or converted in a short time!""",
+        )
     else:
-        return 0
+        if vld is not True:
+            return await m.reply_text(vld)
+        channel_id = m.command[1]
+        if channel_id.startswith("@"):
+            channel_id = channel_id.split("@")[1]
 
-@Client.on_message(filters.private & filters.user(SUDO_USERS) & filters.command('batch'))
-async def batch(client: Client, message: Message):
-    try:
-        first_message = await client.ask(
-            text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
-            chat_id=message.from_user.id,
-            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-            timeout=60
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    "Batch Short 🏕", callback_data=f"batch#{channel_id}"
+                )
+            ],
+            [InlineKeyboardButton("Cancel 🔐", callback_data="cancel")],
+        ]
+
+        return await m.reply(
+            text=f"Are you sure you want to batch short?\n\nChannel: {channel_id}",
+            reply_markup=InlineKeyboardMarkup(buttons),
         )
-    except asyncio.TimeoutError:
-        return
 
-    f_msg_id = await get_message_id(client, first_message)
-    if not f_msg_id:
-        await first_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-        return
 
-    try:
-        second_message = await client.ask(
-            text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
-            chat_id=message.from_user.id,
-            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-            timeout=60
+@Client.on_callback_query(
+    filters.regex(r"^cancel") | filters.regex(r"^batch") & filters.user(SUDO_USERS)
+)
+async def batch_handler(c: Client, m: CallbackQuery):
+    user_id = m.from_user.id
+    user = await get_user(user_id)
+    user_method = user["method"]
+
+    if m.data == "cancel":
+        await m.message.delete()
+        return
+    elif m.data.startswith("batch"):
+        if lock.locked():
+            return await m.answer(
+                "Wait until previous process complete.", show_alert=True
+            )
+
+        channel_id = int(m.data.split("#")[1])
+        try:
+            txt = await c.send_message(channel_id, ".")
+            id = txt.id
+            await txt.delete()
+
+        except ChatWriteForbidden:
+            return await m.message.edit("Bot is not an admin in the given channel")
+        except PeerIdInvalid:
+            return await m.message.edit("Given channel ID is invalid")
+        except Exception as e:
+            logging.exception(e)
+            return await m.message.edit(e)
+
+        start_time = datetime.datetime.now()
+        txt = await m.message.edit(
+            text=f"Batch Shortening Started!\n\n Channel: {channel_id}\n\nTo Cancel /cancel",
         )
-    except asyncio.TimeoutError:
-        return
+        logger.info(f"Batch Shortening Started for {channel_id}")
 
-    s_msg_id = await get_message_id(client, second_message)
-    if not s_msg_id:
-        await second_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-        return
+        success = 0
+        fail = 0
+        total = 0
+        empty = 0
 
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+        total_messages = range(1, id)
+        temp.CANCEL = False
+        try:
+            for i in range(0, len(total_messages), 200):
+                channel_posts = AsyncIter(
+                    await c.get_messages(channel_id, total_messages[i: i + 200])
+                )
+                if temp.CANCEL:
+                    break
+                async with lock:
+                    async for message in channel_posts:
+                        if temp.CANCEL:
+                            break
+                        if message.text or message.caption:
+                            try:
+                                await main_convertor_handler(
+                                    message=message,
+                                    edit_caption=True,
+                                    user=user,
+                                )
+                            except FloodWait as e:
+                                await asyncio.sleep(e.value)
+                                await main_convertor_handler(
+                                    message=message,
+                                    edit_caption=True,
+                                    user=user,
+                                )
 
-@Client.on_message(filters.private & filters.user(SUDO_USERS) & filters.command('genlink'))
-async def link_generator(client: Client, message: Message):
-    try:
-        channel_message = await client.ask(
-            text="Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link",
-            chat_id=message.from_user.id,
-            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-            timeout=60
-        )
-    except asyncio.TimeoutError:
-        return
+                            except Exception as e:
+                                logger.error(e, exc_info=True)
+                                fail += 1
+                            success += 1
+                            await update_stats(message, user_method)
 
-    msg_id = await get_message_id(client, channel_message)
-    if not msg_id:
-        await channel_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote=True)
-        return
+                            await asyncio.sleep(1)
+                        else:
+                            empty += 1
+                        total += 1
 
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+                        if total % 10 == 0:
+                            msg = f"Batch Shortening in Process !\n\nTotal: {total}\nSuccess: {success}\nFailed: {fail}\nEmpty: {empty}\n\nTo cancel the batch: /cancel"
+                            await txt.edit(msg)
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            await m.message.reply(
+                f"Error Occured while processing batch: `{e.message}`"
+            )
+        finally:
+            end_time = datetime.datetime.now()
+            await asyncio.sleep(10)
+            t = end_time - start_time
+            time_taken = str(datetime.timedelta(seconds=t.seconds))
+            msg = f"Batch Shortening Completed!\n\nTime Taken - `{time_taken}`\n\nTotal: `{total}`\nSuccess: `{success}`\nFailed: `{fail}`\nEmpty: `{empty}`"
+            await txt.edit(msg)
+            logger.info(f"Batch Shortening Completed for {channel_id}")
+
+
+@Client.on_message(filters.private & filters.command("cancel") & filters.user(SUDO_USERS))
+async def stop_button(c, m):
+    temp.CANCEL = True
+    msg = await c.send_message(
+        text="<i>Trying To Stoping.....</i>", chat_id=m.chat.id
+    )
+
+    await asyncio.sleep(5)
+    await msg.edit("Batch Shortening Stopped Successfully 👍")
+    logger.info("Batch Shortening Stopped Successfully 👍")
