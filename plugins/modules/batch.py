cd287518cdafa7e84c63acc97f0f24c636500e2c
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-09 17:45:31 +0530

    Update batch.py

--- plugins/modules/batch.py
+++ plugins/modules/batch.py
@@ -1,16 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from plugins import corn
 import base64
 import re
-import asyncio
-from pyrogram import filters
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-from pyrogram.errors import FloodWait
 
-
-SUDO_USERS = [5954494174,5857041668]
-
+SUDO_USERS = [5954494174, 5857041668]
 
 async def encode(string):
     string_bytes = string.encode("ascii")
@@ -27,8 +20,8 @@ async def get_message_id(client, message):
     elif message.forward_sender_name:
         return 0
     elif message.text:
-        pattern = "https://t.me/(?:c/)?(.*)/(\d+)"
-        matches = re.match(pattern,message.text)
+        pattern = r"https://t.me/(?:c/)?(.*)/(\d+)"
+        matches = re.match(pattern, message.text)
         if not matches:
             return 0
         channel_id = matches.group(1)
@@ -42,35 +35,37 @@ async def get_message_id(client, message):
     else:
         return 0
 
-
-
-
 @corn.on_message(filters.private & filters.user(SUDO_USERS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
-    while True:
-        try:
-            first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        f_msg_id = await get_message_id(client, first_message)
-        if f_msg_id:
-            break
-        else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
-            continue
+    try:
+        first_message = await client.ask(
+            text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
+            chat_id=message.from_user.id,
+            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+            timeout=60
+        )
+    except asyncio.TimeoutError:
+        return
 
-    while True:
-        try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        s_msg_id = await get_message_id(client, second_message)
-        if s_msg_id:
-            break
-        else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
-            continue
+    f_msg_id = await get_message_id(client, first_message)
+    if not f_msg_id:
+        await first_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+        return
 
+    try:
+        second_message = await client.ask(
+            text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
+            chat_id=message.from_user.id,
+            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+            timeout=60
+        )
+    except asyncio.TimeoutError:
+        return
+
+    s_msg_id = await get_message_id(client, second_message)
+    if not s_msg_id:
+        await second_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+        return
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
@@ -78,20 +73,22 @@ async def batch(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
-
 @corn.on_message(filters.private & filters.user(SUDO_USERS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
-        else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
-            continue
+    try:
+        channel_message = await client.ask(
+            text="Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link",
+            chat_id=message.from_user.id,
+            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+            timeout=60
+        )
+    except asyncio.TimeoutError:
+        return
+
+    msg_id = await get_message_id(client, channel_message)
+    if not msg_id:
+        await channel_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote=True)
+        return
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
