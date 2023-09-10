9871392b90bd4190edba7d97ea7bddd530b02c85
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-10 12:56:20 +0530

    Update batch.py

--- plugins/modules/batch.py
+++ plugins/modules/batch.py
@@ -141,3 +141,31 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
                 ]
             )
         )
+
+async def reply_forward(message: Message, file_id: int):
+    try:
+        await message.reply_text(
+            f"**Here is Sharable Link of this file:**\n"
+            f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(file_id))}\n\n"
+            f"__To Retrive the Stored File, just open the link!__",
+            disable_web_page_preview=True, quote=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await reply_forward(message, file_id)
+
+async def media_forward(bot: Client, user_id: int, file_id: int):
+    try:
+        if Config.FORWARD_AS_COPY is True:
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id)
+        elif Config.FORWARD_AS_COPY is False:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return media_forward(bot, user_id, file_id)
+
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
+    await asyncio.sleep(2)
