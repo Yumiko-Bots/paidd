d131c0c961af7b68010f9d83671e91661ffd8b6a
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-10 11:06:06 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,20 @@
 from os import path, getenv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+
 
 class config:
 
@@ -6,6 +22,16 @@ class config:
     API_HASH = "06ea2889c5517eb64017b032d667e29f"
     BOT_TOKEN = "6265217004:AAE_yot4SqUFrvBa1IJXgRy-kaeZm9z3Xwk"
 
-    SUDO_USERS = 5954494174
+    SUDO_USERS = 5954494174, 5857041668
     DATABASE_URL = 'mongodb+srv://marcopetrali657:marcopetrali657@cluster0.dwztggn.mongodb.net/?retryWrites=true&w=majority'
     DATABASE_NAME = "marcopetrali657"
+
+    CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
+    CHANNEL_ID = (
+        [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
+        if os.environ.get("CHANNEL_ID")
+        else []
+    )
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    SUDO_USERS.append(OWNER_ID) if OWNER_ID not in SUDO_USERS else []
+
