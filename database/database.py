c4e5f35f61c6aa898c7a0210af769d8f3c337c36
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-10 13:53:04 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,46 +1,77 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from plugins.modules import helpers
-from Config import config
+import datetime
+import motor.motor_asyncio
+from Configs import config as Config
 
 
 class Database:
-    def __init__(self, uri):
-        self._client = AsyncIOMotorClient(uri)
-        self.method = self._client["methods"]
-        self.stats = self._client["stats"]
-        self.users = self._client["users"]
 
-    async def get_db_size(self):
-        return (await self._client.command("dbstats"))["dataSize"]
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
 
-    async def get_bot_stats(self):
-        return await self.stats.find_one({"bot": helpers.temp.BOT_USERNAME})
-
-    async def create_stats(self):
-        await self.stats.insert_one(
-            {
-                "bot": helpers.temp.BOT_USERNAME,
-                "posts": 0,
-                "links": 0,
-                "mdisk_links": 0,
-                "shortener_links": 0,
-            }
+    def new_user(self, id):
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason=''
+            )
         )
 
-    async def update_posts(self, posts: int):
-        myquery = {
-            "bot": helpers.temp.BOT_USERNAME,
-        }
-        newvalues = {"$inc": {"posts": posts}}
-        return await self.stats.update_one(myquery, newvalues)
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
 
-    async def update_links(self, links: int, droplink: int = 0, mdisk: int = 0):
-        myquery = {
-            "bot": helpers.temp.BOT_USERNAME,
-        }
-        newvalues = {
-            "$inc": {"links": links, "mdisk_links": mdisk, "shortener_links": droplink}
-        }
-        return await self.stats.update_one(myquery, newvalues)
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
 
-db = Database(config.DATABASE_URL)
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason):
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('ban_status', default)
+
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({'ban_status.is_banned': True})
+        return banned_users
+
+
+db = Database(Config.DATABASE_URL)
